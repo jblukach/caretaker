e327a6627a5df5fdb0f7185910c8a9d5a3fa0b6c
John Lukach <hello@lukach.io>
2024-01-18 01:55:28 +0000

    stage

--- caretaker/caretaker_stack.py
+++ caretaker/caretaker_stack.py
@@ -10,7 +10,9 @@ from aws_cdk import (
     aws_lambda as _lambda,
     aws_lambda_event_sources as _sources,
     aws_logs as _logs,
-    aws_logs_destinations as _destinations
+    aws_logs_destinations as _destinations,
+    aws_s3 as _s3,
+    aws_s3_deployment as _deployment,
 )
 
 from constructs import Construct
@@ -194,4 +196,403 @@ class CaretakerStack(Stack):
                 table = verify,
                 starting_position = _lambda.StartingPosition.LATEST
             )
+        )
+
+    ### S3 BUCKET ###
+
+        bucket = _s3.Bucket.from_bucket_attributes(
+            self, 'bucket',
+            bucket_arn = 'arn:aws:s3:::stage.tundralabs.org'
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'feodotracker',
+            sources = [
+                _deployment.Source.asset('sources/ip/abusech/feodotracker')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'sslbl',
+            sources = [
+                _deployment.Source.asset('sources/ip/abusech/sslbl')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'alienvault',
+            sources = [
+                _deployment.Source.asset('sources/ip/alienvault')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'binarydefense',
+            sources = [
+                _deployment.Source.asset('sources/ip/binarydefense')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'blocklist',
+            sources = [
+                _deployment.Source.asset('sources/ip/blocklist')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'c2intelfeeds',
+            sources = [
+                _deployment.Source.asset('sources/ip/c2intelfeeds')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'c2tracker',
+            sources = [
+                _deployment.Source.asset('sources/ip/c2tracker')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'cinsscore',
+            sources = [
+                _deployment.Source.asset('sources/ip/cinsscore')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'cybercure',
+            sources = [
+                _deployment.Source.asset('sources/ip/cybercure')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'digitalside',
+            sources = [
+                _deployment.Source.asset('sources/ip/digitalside')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'elliotech',
+            sources = [
+                _deployment.Source.asset('sources/ip/elliotech')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'greensnow',
+            sources = [
+                _deployment.Source.asset('sources/ip/greensnow')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'ipsum',
+            sources = [
+                _deployment.Source.asset('sources/ip/ipsum')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'jamesbrine',
+            sources = [
+                _deployment.Source.asset('sources/ip/jamesbrine')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'miraisecurity',
+            sources = [
+                _deployment.Source.asset('sources/ip/miraisecurity')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'nubinetwork',
+            sources = [
+                _deployment.Source.asset('sources/ip/nubinetwork')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'proofpoint',
+            sources = [
+                _deployment.Source.asset('sources/ip/proofpoint')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'rescure',
+            sources = [
+                _deployment.Source.asset('sources/ip/rescure')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'rutgers',
+            sources = [
+                _deployment.Source.asset('sources/ip/rutgers')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'scorecard',
+            sources = [
+                _deployment.Source.asset('sources/ip/scorecard')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'spamhaus',
+            sources = [
+                _deployment.Source.asset('sources/ip/spamhaus')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'talosintelligence',
+            sources = [
+                _deployment.Source.asset('sources/ip/talosintelligence')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'tor',
+            sources = [
+                _deployment.Source.asset('sources/ip/tor')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'virtualfabric',
+            sources = [
+                _deployment.Source.asset('sources/ip/virtualfabric')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'zonefiles',
+            sources = [
+                _deployment.Source.asset('sources/ip/zonefiles')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'ip',
+            prune = False
+        )
+
+    ### - ###
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'threatfox',
+            sources = [
+                _deployment.Source.asset('sources/dns/abusech/threatfox')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'urlhaus',
+            sources = [
+                _deployment.Source.asset('sources/dns/abusech/urlhaus')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'blackbook',
+            sources = [
+                _deployment.Source.asset('sources/dns/blackbook')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'c2intelfeedsdns',
+            sources = [
+                _deployment.Source.asset('sources/dns/c2intelfeeds')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'certpl',
+            sources = [
+                _deployment.Source.asset('sources/dns/certpl')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'cybercuredns',
+            sources = [
+                _deployment.Source.asset('sources/dns/cybercure')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'digitalsidedns',
+            sources = [
+                _deployment.Source.asset('sources/dns/digitalside')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'openphish',
+            sources = [
+                _deployment.Source.asset('sources/dns/openphish')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'phishingarmy',
+            sources = [
+                _deployment.Source.asset('sources/dns/phishingarmy')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'phishtank',
+            sources = [
+                _deployment.Source.asset('sources/dns/phishtank')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'rescuredns',
+            sources = [
+                _deployment.Source.asset('sources/dns/rescure')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'urlabuse',
+            sources = [
+                _deployment.Source.asset('sources/dns/urlabuse')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'virtualfabricdns',
+            sources = [
+                _deployment.Source.asset('sources/dns/virtualfabric')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
+        )
+
+        copyfiles = _deployment.BucketDeployment(
+            self, 'zonefilesdns',
+            sources = [
+                _deployment.Source.asset('sources/dns/zonefiles')
+            ],
+            destination_bucket = bucket,
+            destination_key_prefix = 'dns',
+            prune = False
         )
