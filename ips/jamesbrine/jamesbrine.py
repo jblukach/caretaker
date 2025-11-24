557dd52edfc6faa647640acb14b54a710b1fb50e
John Lukach <hello@lukach.io>
2025-11-24 00:27:36 +0000

    sources

---
+++ ips/jamesbrine/jamesbrine.py
@@ -0,0 +1,69 @@
+import boto3
+import datetime
+import gzip
+import json
+import os
+import requests
+
+def handler(event, context):
+
+    count = 0
+
+    year = datetime.datetime.now().strftime('%Y')
+    month = datetime.datetime.now().strftime('%m')
+    day = datetime.datetime.now().strftime('%d')
+
+    headers = {'User-Agent': 'Project Caretaker (https://github.com/jblukach/caretaker)'}
+    response = requests.get('https://www.binarydefense.com/banlist.txt', headers=headers)
+    print(f'HTTP Status Code: {response.status_code}')
+    data = response.text
+
+    fname = f'{year}-{month}-{day}-binarydefense.csv'
+    fpath = f'/tmp/{fname}'
+
+    f = open(fpath, 'w')
+    f.write('address,attrib,ts\n')
+
+    for line in data.splitlines():
+        if line.startswith('#'):
+            continue
+        elif line.strip() == '':
+            continue
+        else:
+            f.write(f"{line},1,{year}-{month}-{day}\n")
+            count += 1
+
+    f.close()
+
+    print(f'{count} Addresses')
+
+    s3 = boto3.resource('s3')
+
+    s3.meta.client.upload_file(
+        fpath,
+        os.environ['S3_BUCKET'],
+        'ips/'+fname,
+        ExtraArgs = {
+            'ContentType': "text/csv"
+        }
+    )
+    
+    with open(fpath, 'rb') as f_in:
+        with gzip.open(fpath + '.gz', 'wb') as f_out:
+            f_out.writelines(f_in)
+
+    s3.meta.client.upload_file(
+        fpath + '.gz',
+        os.environ['S3_RESEARCH'],
+        'ips/'+fname+'.gz',
+        ExtraArgs = {
+            'ContentType': "application/gzip"
+        }
+    )
+
+    os.system('ls -lh /tmp')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Completed!')
+    }
