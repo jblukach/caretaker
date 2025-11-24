27e827856d8b3138a8bccb1d9439aaa4ee178844
John Lukach <hello@lukach.io>
2025-11-24 01:13:50 +0000

    sources

--- ips/ultimatehosts/ultimatehosts.py
+++ ips/ultimatehosts/ultimatehosts.py
@@ -13,25 +13,29 @@ def handler(event, context):
     month = datetime.datetime.now().strftime('%m')
     day = datetime.datetime.now().strftime('%d')
 
-    headers = {'User-Agent': 'Project Caretaker (https://github.com/jblukach/caretaker)'}
-    response = requests.get('https://www.binarydefense.com/banlist.txt', headers=headers)
-    print(f'HTTP Status Code: {response.status_code}')
-    data = response.text
-
-    fname = f'{year}-{month}-{day}-binarydefense.csv'
+    fname = f'{year}-{month}-{day}-ultimatehosts.csv'
     fpath = f'/tmp/{fname}'
 
     f = open(fpath, 'w')
     f.write('address,attrib,ts\n')
 
-    for line in data.splitlines():
-        if line.startswith('#'):
-            continue
-        elif line.strip() == '':
-            continue
+    for i in range(20):
+
+        headers = {'User-Agent': 'Project Caretaker (https://github.com/jblukach/caretaker)'}
+        response = requests.get(f'https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/Ultimate.Hosts.Blacklist/refs/heads/master/ips/ips{i}.list', headers=headers)
+        print(f'HTTP Status Code: {response.status_code}')
+        data = response.text
+        
+        if response.status_code == 200:
+            print(f'Processing {i} List')
+            for line in data.splitlines():
+                if line.startswith('#'):
+                    continue
+                else:
+                    f.write(f"{line},25,{year}-{month}-{day}\n")
+                    count += 1
         else:
-            f.write(f"{line},1,{year}-{month}-{day}\n")
-            count += 1
+            break
 
     f.close()
 
