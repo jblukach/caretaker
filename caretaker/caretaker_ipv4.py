1006434b46ae717aa53ef1255c803ccd58e3ab4d
John Lukach <hello@lukach.io>
2025-11-26 02:34:01 +0000

    apis

--- caretaker/caretaker_ipv4.py
+++ caretaker/caretaker_ipv4.py
@@ -19,3 +19,143 @@ class CaretakerIpv4(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+    ### IAM ROLE ###
+
+        role = _iam.Role(
+            self, 'role',
+            assumed_by = _iam.ServicePrincipal(
+                'lambda.amazonaws.com'
+            )
+        )
+
+        role.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'service-role/AWSLambdaBasicExecutionRole'
+            )
+        )
+
+        role.add_to_policy(
+            _iam.PolicyStatement(
+                actions = [
+                    'apigateway:GET'
+                ],
+                resources = [
+                    '*'
+                ]
+            )
+        )
+
+    ### LAMBDA FUNCTION ###
+
+        compute = _lambda.Function(
+            self, 'compute',
+            runtime = _lambda.Runtime.PYTHON_3_13,
+            architecture = _lambda.Architecture.ARM_64,
+            code = _lambda.Code.from_asset('utility/ipv4'),
+            handler = 'ipv4.handler',
+            timeout = Duration.seconds(7),
+            memory_size = 128,
+            role = role
+        )
+
+        logs = _logs.LogGroup(
+            self, 'logs',
+            log_group_name = '/aws/lambda/'+compute.function_name,
+            retention = _logs.RetentionDays.THIRTEEN_MONTHS,
+            removal_policy = RemovalPolicy.DESTROY
+        )
+
+    ### HOSTZONE ###
+
+        hostzoneid = _ssm.StringParameter.from_string_parameter_attributes(
+            self, 'hostzoneid',
+            parameter_name = '/network/hostzone'
+        )
+
+        hostzone = _route53.HostedZone.from_hosted_zone_attributes(
+             self, 'hostzone',
+             hosted_zone_id = hostzoneid.string_value,
+             zone_name = '4n6ir.com'
+        )   
+
+    ### ACM CERTIFICATE ###
+
+        acm = _acm.Certificate(
+            self, 'acm',
+            domain_name = 'ipv4.4n6ir.com',
+            validation = _acm.CertificateValidation.from_dns(hostzone)
+        )
+
+        domain = _api.DomainName(
+            self, 'domain',
+            domain_name = 'ipv4.4n6ir.com',
+            certificate = acm,
+            endpoint_type = _api.EndpointType.REGIONAL,
+            ip_address_type = _api.IpAddressType.DUAL_STACK
+        )
+
+    ### API LOG ROLE ###
+
+        apirole = _iam.Role(
+            self, 'apirole', 
+            assumed_by = _iam.ServicePrincipal(
+                'apigateway.amazonaws.com'
+            )
+        )
+
+        apirole.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'service-role/AmazonAPIGatewayPushToCloudWatchLogs'
+            )
+        )
+
+    ### API GATEWAY ###
+
+        integration = _integrations.HttpLambdaIntegration(
+            'integration', compute
+        )
+
+        api = _api.HttpApi(
+            self, 'api',
+            api_name = 'ipv4',
+            description = 'ipv4.4n6ir.com',
+            default_domain_mapping = _api.DomainMappingOptions(
+                domain_name = domain
+            ),
+            ip_address_type = _api.IpAddressType.DUAL_STACK
+        )
+
+        api.add_routes(
+            path = '/',
+            methods = [
+                _api.HttpMethod.GET
+            ],
+            integration = integration
+        )
+
+    ### DNS RECORDS
+
+        ipv4dns = _route53.ARecord(
+            self, 'ipv4dns',
+            zone = hostzone,
+            record_name = 'ipv4.4n6ir.com',
+            target = _route53.RecordTarget.from_alias(
+                _r53targets.ApiGatewayv2DomainProperties(
+                    domain.regional_domain_name,
+                    domain.regional_hosted_zone_id
+                )
+            )
+        )
+
+        ipv6dns = _route53.AaaaRecord(
+            self, 'ipv6dns',
+            zone = hostzone,
+            record_name = 'ipv4.4n6ir.com',
+            target = _route53.RecordTarget.from_alias(
+                _r53targets.ApiGatewayv2DomainProperties(
+                    domain.regional_domain_name,
+                    domain.regional_hosted_zone_id
+                )
+            )
+        )
