c34d2307c276c83ea9fa55a4fae7dc40f71f1b03
John Lukach <hello@lukach.io>
2025-11-27 01:47:15 +0000

    uniq

--- utility/build/build.py
+++ utility/build/build.py
@@ -7,6 +7,9 @@ def handler(event, context):
     domains = []
     ipv4s = []
     ipv6s = []
+    uniqdns = []
+    uniqipv4 = []
+    uniqipv6 = []
 
     s3 = boto3.client('s3')
 
@@ -26,17 +29,26 @@ def handler(event, context):
                 if parts[0] != 'address':
                     if '.' in parts[0]:
                         ipv4s.append(str(parts[0])+','+str(parts[1]))
+                        uniqipv4.append(str(parts[0]))
                     if ':' in parts[0]:
                         ipv6s.append(str(parts[0])+','+str(parts[1]))
+                        uniqipv6.append(str(parts[0]))
         f.close()
 
     print(f'IPv4s: {len(ipv4s)}')
+    print(f'Unique IPv4s: {len(uniqipv4)}')
     ipv4s = list(set(ipv4s))
+    uniqipv4 = list(set(uniqipv4))
     print(f'Deduplicated: {len(ipv4s)}')
+    print(f'Deduplicated Unique: {len(uniqipv4)}')
+
 
     print(f'IPv6s: {len(ipv6s)}')
+    print(f'Unique IPv6s: {len(uniqipv6)}')
     ipv6s = list(set(ipv6s))
+    uniqipv6 = list(set(uniqipv6))
     print(f'Deduplicated: {len(ipv6s)}')
+    print(f'Deduplicated Unique: {len(uniqipv6)}')
 
     objects = s3.list_objects(
         Bucket = os.environ['S3_BUCKET'],
@@ -53,14 +65,23 @@ def handler(event, context):
                 parts = line.strip().split(',')
                 if parts[0] != 'domain' and '.' in parts[0]:
                     domains.append(str(parts[0])+','+str(parts[1]))
+                    uniqdns.append(str(parts[0]))
         f.close()
 
     print(f'Domains: {len(domains)}')
+    print(f'Unique Domains: {len(uniqdns)}')    
     domains = list(set(domains))
+    uniqdns = list(set(uniqdns))
     print(f'Deduplicated: {len(domains)}')
+    print(f'Deduplicated Unique: {len(uniqdns)}')
 
     with open('/tmp/domains.csv','w') as f:
         for domain in domains:
+            f.write(domain+'\n')
+    f.close()
+
+    with open('/tmp/unique_domains.csv','w') as f:
+        for domain in uniqdns:
             f.write(domain+'\n')
     f.close()
 
@@ -69,8 +90,18 @@ def handler(event, context):
             f.write(ipv4+'\n')
     f.close()
 
+    with open('/tmp/unique_ipv4s.csv','w') as f:
+        for ipv4 in uniqipv4:
+            f.write(ipv4+'\n')
+    f.close()
+
     with open('/tmp/ipv6s.csv','w') as f:
         for ipv6 in ipv6s:
+            f.write(ipv6+'\n')
+    f.close()
+
+    with open('/tmp/unique_ipv6s.csv','w') as f:
+        for ipv6 in uniqipv6:
             f.write(ipv6+'\n')
     f.close()
 
@@ -86,6 +117,15 @@ def handler(event, context):
     )
 
     s3.meta.client.upload_file(
+        '/tmp/unique_domains.csv',
+        os.environ['STAGED_S3'],
+        'unique_domains.csv',
+        ExtraArgs = {
+            'ContentType': "text/csv"
+        }
+    )
+
+    s3.meta.client.upload_file(
         '/tmp/ipv4s.csv',
         os.environ['STAGED_S3'],
         'ipv4s.csv',
@@ -95,9 +135,27 @@ def handler(event, context):
     )
 
     s3.meta.client.upload_file(
+        '/tmp/unique_ipv4s.csv',
+        os.environ['STAGED_S3'],
+        'unique_ipv4s.csv',
+        ExtraArgs = {
+            'ContentType': "text/csv"
+        }
+    )
+
+    s3.meta.client.upload_file(
         '/tmp/ipv6s.csv',
         os.environ['STAGED_S3'],
         'ipv6s.csv',
+        ExtraArgs = {
+            'ContentType': "text/csv"
+        }
+    )
+
+    s3.meta.client.upload_file(
+        '/tmp/unique_ipv6s.csv',
+        os.environ['STAGED_S3'],
+        'unique_ipv6s.csv',
         ExtraArgs = {
             'ContentType': "text/csv"
         }
