be06f202d4db40a9932fc87d1e05f7d8936f36d2
John Lukach <hello@lukach.io>
2025-11-28 01:53:21 +0000

    remove

--- caretaker/caretaker_stack.py
+++ caretaker/caretaker_stack.py
@@ -5,7 +5,6 @@ from aws_cdk import (
     aws_glue_alpha as _glue,
     aws_iam as _iam,
     aws_s3 as _s3,
-    aws_s3_deployment as _deployment,
     aws_ssm as _ssm
 )
 
@@ -77,13 +76,6 @@ class CaretakerStack(Stack):
             auto_delete_objects = True,
             enforce_ssl = True,
             versioned = False
-        )
-
-        deployment = _deployment.BucketDeployment(
-            self, 'deployment',
-            sources = [_deployment.Source.asset('code')],
-            destination_bucket = staged,
-            prune = False
         )
 
         bucket_policy = _iam.PolicyStatement(
